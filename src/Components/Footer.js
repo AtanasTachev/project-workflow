ab737c1b8af2a730c73c40e31ba6c9810f52e197
Atanas Tachev <tachev_a#yahoo.com>
2021-11-08 07:15:42 +0200

    initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import '../App.css';
+
+function Footer () {
+    return (<nav className="footer">
+        <p>All rights reserved &copy;</p>
+    </nav>);
+}
+
+export default Footer
---
+++ src/Components/Navigation.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function Navigation () {
+    return (<nav className="nav">
+        <a className="atag">Search</a>
+        <a className="atag">Login</a>
+        <a className="atag">Register</a>
+        <a className="atag">My profile</a>
+    </nav>);
+}
+
+export default Navigation
