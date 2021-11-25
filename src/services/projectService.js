ea33c6a10aed0c8f3e741c3ff3772d4008674341
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2021-11-26 00:00:23 +0200

    change mode to no-cors

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -11,6 +11,7 @@ export const getAll = async () => {
 
 export const create = async (projectData) => {
     let response = await fetch(`${baseUrl}/projects`, {
+        mode: 'no-cors',
         method: 'POST',
         headers: {
             'content-type': 'application/json'
